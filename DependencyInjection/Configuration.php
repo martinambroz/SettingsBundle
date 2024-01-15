83bfd7e0285c9e71998cd510c83742277e493de0
Martin Ambrož <martinambroz@users.noreply.github.com>
2024-01-15 19:49:10 +0100

    Update Configuration.php
    Symfony 6.X deprecation hunt.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('dmishh_settings');
         // Keep compatibility with symfony/config < 4.2
